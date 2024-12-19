5ecec0eb6294d19a1134b606452b3779a5884a2d
Cyril Fauveau <fauveaucyril@gmail.com>
2024-12-19 16:17:01 +0100

    :update: adapt to sepolia contract

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const contractAddress = "0x3E0e97289beC05e151E97739c1833074b5ea09f1"
 export const contractAbi = [
   {
     "inputs": [],
